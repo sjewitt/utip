a2ddc8b9121bd0e623167c7abab56183db4e1ece
silas <silas@thecoven.co.uk>
2025-11-10 11:41:41 +0000

    named args tests in scratchpad

--- lib/scratchpad.dart
+++ lib/scratchpad.dart
@@ -14,24 +14,51 @@ void main() {
   print(cod.getType());
 
   // from course:
-  Bob bob = Bob(123, "FISH");
-  Bob bob2 = Bob(1234432, "FISH and CHIPS");
-  var age = bob.age = 412;
+  // Bob bob = Bob(123, "FISH");
+  // Bob bob2 = Bob(1234432, "FISH and CHIPS");
+
+  // 6:19:
+  Bob bob3 = Bob(age: 1, name: "BOB 1");
+  Bob bob4 = Bob(name: "BOB 2", age: 3);
+  Bob bob5 = Bob(age: 34);
+  // Bob bob6 = Bob(name: "sausage"); // errors as expected.
+  // var age = bob.age = 412;
   // print("BOB: ${bob.name} ${bob.age}");
-  bob.greeting();
-  bob2.greeting();
+  bob3.greeting();
+  bob4.greeting();
+  bob5.greeting();
+  bob5.info(showage: true);
+  bob5.info(showname: true);
+  bob5.info(showage: true, showname: true);
 }
 
+// 6.19 named args:
 class Bob {
   int age;
   String name;
 
   // constructor
-  Bob(this.age, this.name);
+  // Bob(age, name);
+  // Bob(this.age, this.name); // why does this work?
+
+  // 6.19: see https://stackoverflow.com/questions/72235859/can-required-parameters-in-a-dart-constructor-be-named
+  Bob({required this.age, this.name = "mr no-name"});
 
   // behaviour
   void greeting() {
     print("BOB method: ${this.name} ${this.age}"); // `this` is optional
+  }
+
+  void info({showname = false, showage = false}) {
+    if (showname) {
+      print("NAME: $name ${this.name}");
+    }
+    if (showname && showage) {
+      print("AND...");
+    }
+    if (showage) {
+      print("AGE: $age");
+    }
   }
 }
 
