b9f208e883ced414a3c7674673c6bfd879107af1
silas <silas@thecoven.co.uk>
2025-11-10 16:48:19 +0000

    abstracted bill amout textfield, begin adding mickey mouse maths (which works mind\!) to calculate tip PP. Needs to be converted to currency (to 2dp, rounding up) and comments tidied up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// see https://stackoverflow.com/questions/75097840/is-double-and-double-are-different-thing-in-dart-and-if-it-is-can-anyone-explain
+// import 'dart:ffi';  // contains Double. We actually want `double`
+
 import 'package:flutter/material.dart';
+import 'package:utip/widgets/bill_amt_text_field.dart';
 import 'package:utip/widgets/person_counter.dart';
 import 'package:utip/widgets/tip_percent_slider.dart';
-import 'package:utip/widgets/test_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,6 +42,8 @@ class _UTipState extends State<UTip> {
   double sliderPos = 0;
   double tipPercentPerPerson = 0;
 
+  double tipPerPerson = 0;
+
   // todo:
   double totalCost = 0.0;
 
@@ -67,6 +72,35 @@ class _UTipState extends State<UTip> {
       debugPrint("setting to sliderValue of: $sliderValue...");
       // I DO need all three vars - the TPP is also the tooltip!
       tipPercentPerPerson = sliderPos = sliderValue;
+    });
+  }
+
+  // handler for total cost (blur?, on personcounter change?)
+  // `value` comes from the input field
+  void handleBillAmount(value) {
+    setState(() {
+      if (value is String) {
+        debugPrint("Its a trap!");
+        // This'll need exception handling:
+        value = double.parse(value); // hmm... double vs Double types...
+      }
+      if (value is double) {
+        debugPrint("Its a sausage! $value");
+      }
+
+      /**
+       * It's now a float, so we can work out the amount PP. It will probably 
+       * need to be a Currency type.
+       */
+      totalCost = value;
+      debugPrint("Bill amount is \$$totalCost");
+      debugPrint("There are $personCount guests");
+      debugPrint("Tip percentage is $sliderPos");
+      debugPrint("Amount per person: ${value / personCount}");
+      tipPerPerson = (value * (sliderPos / 100)) / personCount;
+      debugPrint("Tip amount per person: $tipPerPerson");
+
+      // TODO: trigger change from all widgets.
     });
   }
 
@@ -101,7 +135,7 @@ class _UTipState extends State<UTip> {
               children: [
                 Text("Total per person:", style: style),
                 Text(
-                  "\$20.99",
+                  "\$$tipPerPerson",
                   style: style.copyWith(
                     fontSize: theme.textTheme.displaySmall!.fontSize,
                   ),
@@ -112,6 +146,7 @@ class _UTipState extends State<UTip> {
           Padding(
             padding: const EdgeInsets.all(3.0),
             child: Container(
+              padding: EdgeInsets.all(12),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
                 border: Border.all(color: theme.colorScheme.primary, width: 2),
@@ -119,22 +154,18 @@ class _UTipState extends State<UTip> {
 
               child: Column(
                 children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.attach_money),
-                      labelText: "Total cost:",
-                    ),
-                    keyboardType: const TextInputType.numberWithOptions(
-                      decimal: true,
-                    ),
-                    onChanged: (String value) {
-                      // call handler here to set the totalCost var
-                      debugPrint(
-                        "VARS: $value, $personCount, $tipPercentPerPerson",
-                      );
-                    },
+                  // bill amout widget
+                  BillAmtTextField(
+                    // he also passes in the value?
+                    // the value is implicit in the args...
+
+                    // mine
+                    personCount: personCount,
+                    tipPercentPerPerson: tipPercentPerPerson,
+                    handleBillAmount: handleBillAmount, // the handler function
                   ),
+
+                  // person counter widget
                   PersonCounter(
                     theme: theme,
                     personCount: personCount,
