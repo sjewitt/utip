6e9be60f0f2a2b6b4e7cb5851cbf12e6d37ff67b
silas <silas@thecoven.co.uk>
2025-11-11 12:46:03 +0000

    not tidied, adds comparisons wit the course notes. Main one being where the vars are declared.

--- lib/main.dart
+++ lib/main.dart
@@ -44,18 +44,26 @@ class _UTipState extends State<UTip> {
   double tipPercentPerPerson = 0;
 
   double tipPerPerson = 0;
+  double tipTotal = 0;
   double costPerPerson = 0;
 
   // todo:
   double totalCost = 0.0;
 
+  // see https://medium.com/@varsha.vikshith.tech/format-currency-like-a-pro-in-dart-flutter-15a183668241
   String finalCostPerPersonOutput = "0.00";
-  String finalOutput = "0.00";
+  String finalTipPerPersonOutput = "0.00";
+  String finalTipTotalOutput = "0.00";
 
   // person counter handlers:
   void decrementCounter() {
     setState(() {
-      if (personCount > 0) personCount--;
+      // 6.22 - he corrects to if personcount > 1, so we always get at least
+      //one. I think I handled this case lower down in handleBillAmount()
+      // it actually makes logical sense to stop at 1, not zero. You can't
+      // really have zero guests...
+      // and doing this probably means I can simplify the checks later on too
+      if (personCount > 1) personCount--;
       debugPrint("Current bill: $totalCost");
       debugPrint("Current guest count: $personCount");
       // and as I have the totalCost - which is the same as the text field
@@ -99,6 +107,30 @@ class _UTipState extends State<UTip> {
     });
   }
 
+  // for 6.22, he does this:
+  // ARGH!! - I did think about this yesterday. We need to ADD THE TIP TO THE BILL!
+  double totalPerPerson() {
+    // don't forget, I made the slider return the percentage, NOT the float!
+    double _total =
+        ((totalCost * (tipPercentPerPerson / 100)) + (totalCost)) /
+        (personCount); // oerator precedence!! WTF?
+    debugPrint("HIS METHOD: TOTAL BILL: $_total");
+    debugPrint("HIS METHOD: totalCost: $totalCost");
+    debugPrint("HIS METHOD: tipPercentPerPerson: $tipPercentPerPerson");
+    debugPrint("HIS METHOD: personCount: $personCount");
+
+    return (_total);
+  }
+
+  // and he does THIS for getting the tip value:
+  double totalTip() {
+    double _tip = ((totalCost * tipPercentPerPerson) / 100);
+    debugPrint("HIS METHOD: TOTAL TIP: $_tip");
+    debugPrint("HIS METHOD: totalCost: $totalCost");
+    debugPrint("HIS METHOD: tipPercentPerPerson: $tipPercentPerPerson");
+    return (_tip);
+  }
+
   // handler for total cost (blur?, on personcounter change?)
   // `value` comes from the input field
   void handleBillAmount(totalBillAmount) {
@@ -120,6 +152,9 @@ class _UTipState extends State<UTip> {
        * It's now a float, or default of 0, so we can work out the amount PP. It will probably 
        * need to be a Currency type.
        */
+      // see comment in decrementCounter()
+      // but I check for numm here ANYWAY, because `totalBillAmount = double.parse(totalBillAmount,);`
+      // could return `null`
       if (totalBillAmount != null) {
         totalCost = totalBillAmount;
         debugPrint("Bill amount is \$$totalBillAmount");
@@ -128,6 +163,8 @@ class _UTipState extends State<UTip> {
         if (personCount > 0) {
           costPerPerson = totalBillAmount / personCount;
           debugPrint("Amount per person: $costPerPerson");
+          tipTotal =
+              totalBillAmount * (sliderPos / 100); // added to account for 6.22
           tipPerPerson = (totalBillAmount * (sliderPos / 100)) / personCount;
           debugPrint("Tip amount per person: $tipPerPerson");
         } else {
@@ -135,14 +172,23 @@ class _UTipState extends State<UTip> {
         }
       } else {
         tipPerPerson = 0.0;
+        tipTotal = 0.0;
       }
 
       // and format the tip per person as currency:
       // https://stackoverflow.com/questions/14865568/currency-format-in-dart
       final formatCurrency = NumberFormat.simpleCurrency(locale: "en_GB");
-      finalOutput = formatCurrency.format(tipPerPerson);
-      finalCostPerPersonOutput = formatCurrency.format(costPerPerson);
-      debugPrint("Tip amount per person: $finalOutput");
+      // 6.22 - he actually puts the TOTAL tip on the screen not the TPP:
+
+      finalTipPerPersonOutput = formatCurrency.format(tipPerPerson);
+      finalTipTotalOutput = formatCurrency.format(tipTotal);
+      finalCostPerPersonOutput = formatCurrency.format(
+        costPerPerson + tipPerPerson,
+      );
+      debugPrint("Tip amount total: $finalTipTotalOutput");
+      debugPrint("Tip amount per person: $finalTipPerPersonOutput");
+      // and his implementation:
+      totalPerPerson();
       // TODO: trigger change from all widgets. - DONE
     });
   }
@@ -150,6 +196,13 @@ class _UTipState extends State<UTip> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+
+    // 6.22 - he sets a var HERE:
+    // This is the same as my `finalCostPerPersonOutput` var
+    // so I have the wrong scope..
+    // but hey this is calling the same underlying method :-)
+    double total = totalPerPerson();
+    double totalT = totalTip();
 
     final style = theme.textTheme.titleMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
@@ -178,8 +231,14 @@ class _UTipState extends State<UTip> {
               children: [
                 Text("Total per person:", style: style),
                 Text(
-                  // "\$$tipPerPerson",
+                  // "\$$tipPerPerson",finalCostPerPersonOutput
                   finalCostPerPersonOutput, // this is a formatted currency string
+                  style: style.copyWith(
+                    fontSize: theme.textTheme.displaySmall!.fontSize,
+                  ),
+                ),
+                Text(
+                  "\$$total", // his version
                   style: style.copyWith(
                     fontSize: theme.textTheme.displaySmall!.fontSize,
                   ),
@@ -200,7 +259,8 @@ class _UTipState extends State<UTip> {
                 children: [
                   // bill amout widget
                   BillAmtTextField(
-                    // he also passes in the value?
+                    // he also passes in the value? with a toString()
+                    // Hmm...
                     // the value is implicit in the args...
 
                     // mine
@@ -223,7 +283,15 @@ class _UTipState extends State<UTip> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text("Tip", style: theme.textTheme.titleMedium),
-                      Text(finalOutput), // this is a formatted currency string
+                      Text(
+                        // finalTipPerPersonOutput,
+                        // or:
+                        finalTipTotalOutput, // my amended. 6.22
+                      ), // this is a formatted currency string
+                      // 6.22 - and his: totalT
+                      // Text(
+                      //   "XX $totalT",
+                      // ), // he also buggers about with type elsewhere
                     ],
                   ),
                   // slider text:
